03be56a793b8f1a2858be9550875fef3b2c83f4a
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-19 14:48:04 -0400

    edited port/username/password to function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,31 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port;
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+  password: "password",
+  database: "bamazon_DB"
+    });
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        runSearch();
+    });
+
+    function runSearch() {
+        inquirer
+        .prompt({
+            name: 
+            type:
+            message:
+            choices:
+        })
+    }
