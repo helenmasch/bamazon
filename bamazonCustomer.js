f574040e07f53d26f8672adffdc3eeecee8e5b7c
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-19 15:43:08 -0400

    added connection function and sql document in files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,14 +17,25 @@ var connection = mysql.createConnection({
 
     connection.connect(function(err) {
         if (err) throw err;
-        runSearch();
+        console.log("connected as id " + connection.threadId + "\n");
+        readProducts();
     });
+
+    function readProducts() {
+        connection.query("SELECT id FROM products", function(err, res) {
+            if (err) throw err;
+
+            // Log results of the SELECT statement
+            console.log(res);
+            connection.end();
+        })
+    }
 
     function runSearch() {
         inquirer.prompt({
-            name: 
-            type:
-            message:
-            choices:
+            name: "choose",
+            type: "list",
+            message: "What is the ID of the product you would like to buy?",
+            choices: ["1" , "2", "3", "4", "5", "6", "7" , "8" , "9" , "10"]
         })
     }
