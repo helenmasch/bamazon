04ccd25572beccb9771b512376f1574ac2c8c16a
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-19 18:26:53 -0400

    fixed bugs and able to get the app running correctly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+require("console.table")
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -11,31 +11,48 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
-  password: "password",
-  database: "bamazon_DB"
-    });
+    password: "password123",
+    database: "bamazon_DB"
+});
 
-    connection.connect(function(err) {
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    readProducts();
+});
+
+function readProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log("connected as id " + connection.threadId + "\n");
-        readProducts();
-    });
 
-    function readProducts() {
-        connection.query("SELECT id FROM products", function(err, res) {
-            if (err) throw err;
+        // Log results of the SELECT statement
+        console.log(res);
 
-            // Log results of the SELECT statement
-            console.log(res);
-            connection.end();
+        console.table(res)
+      runSearch()
+        // connection.end();
+    })
+}
+
+function runSearch() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the ID of the product you would like to buy?",
+        name: "itemID"
+    },
+        {
+            type: "input",
+            message: "How many units do you want to buy?",
+            name: "units"
+        }
+    ]).then(function (data) {
+        console.log(data)
+        connection.query("SELECT * FROM products WHERE id ="+ data.itemID, function(err, results){
+            console.table(results)
+            var newQuantity = results[0].stock_quantity - data.units
+            
+            connection.query("UPDATE * SET products WHERE id ")
+
         })
-    }
-
-    function runSearch() {
-        inquirer.prompt({
-            name: "choose",
-            type: "list",
-            message: "What is the ID of the product you would like to buy?",
-            choices: ["1" , "2", "3", "4", "5", "6", "7" , "8" , "9" , "10"]
-        })
-    }
+    })
+}
